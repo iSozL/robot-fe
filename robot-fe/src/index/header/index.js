ae0a6f0f8d642b2ec8cb64a0251f8c67ef913874
jello <2511841570@qq.com>
2019-08-10 15:41:05 +0800

    :art: add logout

--- robot-fe/src/index/header/index.js
+++ robot-fe/src/index/header/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./index.css";
+import { Menu, Dropdown } from "antd";
 class Head extends React.Component {
   constructor(props) {
     super(props);
   }
+  logout() {
+    localStorage.clear();
+    window.location.href = "/login";
+  }
   render() {
+    const menu = (
+      <Menu>
+        <Menu.Item key="0">
+          <div onClick={this.logout}>退出</div>
+        </Menu.Item>
+      </Menu>
+    );
     return (
       <div>
-        <div className="header-title">
-          浩浩机器人
-        </div>
+        <div className="header-title">浩浩机器人</div>
         <div className="userInfo">
-          <img src={window.localStorage.getItem('userImage')} />{window.localStorage.getItem('username')}
+          <Dropdown overlay={menu}>
+            <img src={window.localStorage.getItem("userImage")} />
+          </Dropdown>
+          {window.localStorage.getItem("username")}
         </div>
       </div>
     );
